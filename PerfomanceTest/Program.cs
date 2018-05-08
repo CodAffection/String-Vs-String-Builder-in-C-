ff94de4a236ad0f6bbe730f6a8e66fd34262bd6a
DotnetMob <dotnetmob@gmail.com>
2018-05-08 06:10:24 +0530

    initial

---
+++ PerfomanceTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Diagnostics;
+
+namespace PerfomanceTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch Mytimer = new Stopwatch();
+            string str = string.Empty;
+            Mytimer.Start();
+            for (int i = 0; i < 10000; i++)
+            {
+                str += i.ToString();
+            }
+            Mytimer.Stop();
+            Console.WriteLine("Time taken by string : {0}", Mytimer.Elapsed);
+
+            StringBuilder sbr = new StringBuilder(string.Empty);
+            //restart timer from zero
+            Mytimer.Restart();
+            for (int i = 0; i < 10000; i++)
+            {
+                sbr.Append(i.ToString());
+            }
+            Mytimer.Stop();
+            Console.WriteLine("Time taken by stringbuilder : {0}", Mytimer.Elapsed);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StringVsStringBuilder/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringVsStringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //String concatenation using string class
+            String str = "I am ";//1000H
+            str += "passionate about ";//2000H
+            str += "programming";//3000H
+            Console.WriteLine("Result from string :" + str);
+
+
+            //String concatenation using stringbuilder class
+            StringBuilder strb = new StringBuilder("I am ");//5000H
+            strb.Append("passionate about ");
+            strb.Append("programming");
+            Console.WriteLine("Result from stringbuilder :" + strb);
+
+            Console.ReadKey();
+        }
+    }
+}
